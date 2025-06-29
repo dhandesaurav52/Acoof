44d1f009ac88507945b6a50bdd97b2726aecb618
saurav dhande <dhandesaurav52@gmail.com>
2025-06-29 17:10:51 +0000

    this is the logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export function Logo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 1000 300"
+      viewBox="0 0 900 350"
       className={className}
       {...props}
       xmlns="http://www.w3.org/2000/svg"
@@ -11,33 +11,55 @@ export function Logo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
       <defs>
         <style>
           {`
-            .logo-text {
+            .acoof-text {
               font-family: 'Montserrat', sans-serif;
-              font-weight: 900;
-              font-size: 180px;
-              fill: currentColor;
-              letter-spacing: -10px;
+              font-weight: 800;
+              font-size: 200px;
+              fill: white;
+            }
+            .swaz-text {
+              font-family: 'Montserrat', sans-serif;
+              font-weight: 600;
+              font-size: 50px;
+              fill: white;
+            }
+            .in-text {
+              font-family: 'Montserrat', sans-serif;
+              font-weight: 800;
+              font-size: 100px;
+              fill: black;
             }
           `}
         </style>
       </defs>
 
-      {/* Background shape */}
-      <path
-        d="M0 50 Q 50 0, 100 50 L 100 250 Q 50 300, 0 250 Z"
-        fill="black"
-      />
-      <path
-        d="M900 50 Q 950 0, 1000 50 L 1000 250 Q 950 300, 900 250 Z"
-        fill="black"
-      />
+      {/* Red background */}
+      <rect width="900" height="350" fill="hsl(var(--primary))" />
 
-      {/* Text "Acoof" */}
-      <text x="120" y="210" className="logo-text">Acoof</text>
+      {/* Text "AC" */}
+      <text x="20" y="250" className="acoof-text">AC</text>
+      
+      {/* Text "SWAZ" inside C */}
+      <text x="215" y="180" className="swaz-text">SWAZ</text>
 
-      {/* Red accent lines */}
-      <line x1="20" y1="150" x2="80" y2="150" stroke="hsl(var(--primary))" strokeWidth="20" strokeLinecap="round" />
-      <line x1="920" y1="150" x2="980" y2="150" stroke="hsl(var(--primary))" strokeWidth="20" strokeLinecap="round" />
+      {/* Shopping Bag */}
+      <g transform="translate(380, 50)">
+        {/* Bag body */}
+        <path d="M0,20 L240,20 L210,240 L30,240 Z" fill="black" />
+        {/* Bag handle */}
+        <path d="M70,20 C70,-40 170,-40 170,20" stroke="black" strokeWidth="25" fill="none" />
+        {/* Eyes (OO) */}
+        <circle cx="85" cy="120" r="40" fill="white" />
+        <circle cx="155" cy="120" r="40" fill="white" />
+        {/* Smile */}
+        <path d="M80,180 Q120,220 160,180" stroke="white" strokeWidth="12" fill="none" strokeLinecap="round" />
+      </g>
+
+      {/* Text "F" */}
+      <text x="610" y="250" className="acoof-text">F</text>
+      
+      {/* Text ".in" */}
+      <text x="750" y="250" className="in-text">.in</text>
 
     </svg>
   );
