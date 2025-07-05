9110a193d9cccbdec4eda524d08ef1ff10673cb3
saurav dhande <dhandesaurav52@gmail.com>
2025-07-05 13:00:03 +0000

    The logo is squared not rectangular

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,3 @@
-
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
@@ -7,14 +6,13 @@ export function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElemen
     // The parent element will control the size via the className.
     // We add an aspect ratio to the container so that `fill` works correctly
     // even if only one dimension (e.g., height) is specified in the className.
-    <div className={cn('relative aspect-[260/80]', className)} {...props}>
+    <div className={cn('relative aspect-square', className)} {...props}>
       <Image
-        src="https://placehold.co/260x80.png"
+        src="https://firebasestorage.googleapis.com/v0/b/genkit-llm-76571.appspot.com/o/1474921b-683a-4dd3-9114-152e04b43445?alt=media&token=c27c6536-6c8c-49f4-ac3e-5b1233075c32"
         alt="White Wolf Logo"
         fill
         priority
         className="object-contain"
-        data-ai-hint="wolf logo"
       />
     </div>
   );
