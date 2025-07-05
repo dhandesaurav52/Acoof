c543873a16af142243211fdf249e6719c9d2ff1b
saurav dhande <dhandesaurav52@gmail.com>
2025-07-05 13:12:28 +0000

    https://firebasestorage.googleapis.com/v0/b/acoof-8e92d.firebasestorage.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -8,7 +8,7 @@ export function Logo({ className, ...props }: React.HTMLAttributes<HTMLDivElemen
     // even if only one dimension (e.g., height) is specified in the className.
     <div className={cn('relative aspect-square', className)} {...props}>
       <Image
-        src="https://firebasestorage.googleapis.com/v0/b/acoof-8e92d.appspot.com/o/74c364fd-44c0-4461-873a-9ac7b928858f.png?alt=media"
+        src="https://firebasestorage.googleapis.com/v0/b/acoof-8e92d.firebasestorage.app/o/74c364fd-44c0-4461-873a-9ac7b928858f.png?alt=media&token=80f76b0f-9511-4ed7-90f1-1808bf810e37"
         alt="White Wolf Logo"
         fill
         priority
