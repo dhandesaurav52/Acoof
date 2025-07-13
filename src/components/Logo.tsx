44bf79097748ff854e7702abcf33419790af38a5
saurav dhande <dhandesaurav52@gmail.com>
2025-07-13 05:07:36 +0000

    please use this image as our App logo in the header and footer https://f

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ width, height, className }: { width: number; height: numb
   return (
     <div className={cn('relative', className)} style={{ width, height }}>
       <Image
-        src="https://firebasestorage.googleapis.com/v0/b/acoof-8e92d.appspot.com/o/Acoof%20Logo.png?alt=media&token=72b9e983-2eb5-4322-b1a6-b3a0e0500789"
+        src="https://firebasestorage.googleapis.com/v0/b/acoof-8e92d.firebasestorage.app/o/Acoof%20Logo.png?alt=media&token=a722f806-57c7-409a-ac91-6d7dff0f9a67"
         alt="Urban Attire Lookbook Logo"
         fill
         priority
